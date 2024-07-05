9f33077bbf2f8fce83587d4eb94b97bf85003ef1
shlomota <shlomotannor@mail.tau.ac.il>
2024-07-04 22:09:52 -0400

    update midis dir to /data/midis

--- consts.py
+++ consts.py
@@ -21,7 +21,7 @@ MIDIS_COLLECTION = CHROMA_CLIENT.get_or_create_collection("midi_chunks", metadat
 
 SAMPLE_QUERIES_DIR = "/home/ubuntu/MeloDetective/data/sample_queries"
 LIBRARY_DIR = "/home/ubuntu/MeloDetective/data/library"
-MIDIS_DIR = "/home/ubuntu/MeloDetective/data/midis2"
+MIDIS_DIR = "/home/ubuntu/MeloDetective/data/midis"
 METADATA_DIR = "/home/ubuntu/MeloDetective/data/metadata"
 LOG_DIR = "logs"
 CHUNKS_DIR = "/home/ubuntu/MeloDetective/data/chunks"
